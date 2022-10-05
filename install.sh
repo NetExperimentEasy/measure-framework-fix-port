551ad3bd88e055808a67c5536dc7162aa1c5ae62
derekwin <1446103183@qq.com>
2022-10-05 11:40:35 +0800

    update

---
+++ install.sh
@@ -0,0 +1,10 @@
+apt-get update
+apt-get install -y python3-pip ethtool netcat moreutils
+pip3 install -r requirements.txt
+
+# this fixes mininet bug with ovs-controller
+apt-get install -y openvswitch-testcontroller
+cp /usr/bin/ovs-testcontroller /usr/bin/ovs-controller
+
+# load bbr kernel module
+modprobe tcp_bbr
