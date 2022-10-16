da7839df86c401b48414bc521ecd79c952f69846
JaceLau <1446103183@qq.com>
2022-10-16 20:31:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ apt-get install -y python3-pip ethtool netcat moreutils
 pip3 install -r requirements.txt
 
 # this fixes mininet bug with ovs-controller
-apt-get install -y openvswitch-testcontroller
-cp /usr/bin/ovs-testcontroller /usr/bin/ovs-controller
+# apt-get install -y openvswitch-testcontroller
+# cp /usr/bin/ovs-testcontroller /usr/bin/ovs-controller
 
 # load bbr kernel module
 modprobe tcp_bbr
